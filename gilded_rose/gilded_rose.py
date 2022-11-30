d11ffbd8be1fa0784d49c79aa849e76993585456
bmerema <bart.merema@deltaq.io>
2022-11-30 20:48:17 +0100

    updated items now

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
 
+class GildedRose(object):
+    max_quality = 50
+    min_quality = 0
     def __init__(self, items):
         self.items = items
 
@@ -9,28 +11,53 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
-            if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 1 and item.quality < 50:
-                    item.quality = item.quality + 1
-
+            elif item.name == "Aged Brie":
+                self.update_brie(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 11 and item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6 and item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 1:
-                    item.quality = item.quality - item.quality
+                self.update_backstage_pass(item)
+            elif item.name == "Conjured Mana Cake":
+                self.update_conjured(item)
             else:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-                if item.sell_in < 1 and item.quality > 0:
-                    item.quality = item.quality - 1
-            item.sell_in = item.sell_in - 1
-  
+                self.item_update(item)
+
+    def item_update(self, item):
+        if item.quality > self.min_quality:
+            item.quality = item.quality - 1
+            # if sell in days have passed, decrease quality twice
+            if item.sell_in <= 0:
+                item.quality = item.quality - 1
+        item.sell_in = item.sell_in - 1
+
+
+    def update_brie(self, item):
+        if item.quality < self.max_quality:
+            item.quality = item.quality + 1
+            if item.sell_in <= 0:
+                item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+
+    def update_backstage_pass(self, item):
+        if 10 >= item.sell_in > 5:
+            item.quality = item.quality + 2
+        elif 5 >= item.sell_in > 0:
+            item.quality = item.quality + 3
+        elif item.sell_in <= 0:
+            item.quality = 0
+        else:
+            item.quality = item.quality + 1
+
+        if item.quality > self.max_quality:
+            item.quality = self.max_quality
+        item.sell_in = item.sell_in - 1
+
+    def update_conjured(self, item):
+        if item.quality > self.min_quality:
+            item.quality = item.quality - 2
+            if item.sell_in <= 0:
+                item.quality = item.quality - 2
+        if item.quality < self.min_quality:
+            item.quality = self.min_quality
+        item.sell_in = item.sell_in - 1
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -39,3 +66,13 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    # def item_update(normal_item: Item):
+    #     if min_quality < normal_item.quality: = 0 else normal_item.quality: -= 2
+    #     normal_item.quality = max(normal_item.quality, min_quality)
+    #     normal_item.sell_in -= 1
+    #
+    # def aged_brie_updater(aged_brie: Item):
+    #     if min_quality <= aged_brie.quality: else aged_brie.quality + 2:
+    #         aged_brie.quality = min(max_quality, quality)
+    #     aged_brie.sell_in -= 1
--- gilded_rose/test_gilded_rose.py
+++ gilded_rose/test_gilded_rose.py
@@ -30,7 +30,14 @@ class GildedRoseTest(unittest.TestCase):
         item = items[0]
         self.assertEqual(item.quality, expected['quality'])
         self.assertEqual(item.sell_in, expected['sell_in'])
-
+    def test_sulfuras(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 20, 80)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", item.name)
+        self.assertNotEqual(item.sell_in, 15)
+        self.assertEqual(item.sell_in, 20)  # does not change
+        self.assertEqual(item.quality, 80)  # never alters
 
 # def test_quality
 # def test_aged_brie
